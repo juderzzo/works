3a4e55446dded067a0108b651664bde7e40daf53
Jude Rizzo <jrizzo00@stuy.edu>
2019-09-23 23:29:31 -0400

    leon

--- 09_stat/sofdevsite.py
+++ 09_stat/sofdevsite.py
@@ -1,20 +1,29 @@
+#Jude Rizzo
+#Sofdev1 pd9
+#K09 -- ’Tis Not a Race -- But You Will Go Faster
+#2019--9--19
 from flask import Flask
 app = Flask(__name__)
 #print("Printing this in the flask console")
+
+#you set a route with the decorator, which is marked by an @
+#each one seems to have one fcuntion under it
 @app.route("/")
-@app.route("/..")
-@app.route("/../../")
-
-
 def hello_world():
   print(__name__)
+  return  "Call me king, cuz I be k"
 
-  return  "1,2,3"
+ #also every string in the decorator begins with a /
 
+@app.route("/static/template.html")
+#in class assignment
+def template():
+    x = [0,1,1,2,3,5,8]
+    return "coll = " + str(x)
 
-
-def printStuff():
-    return "<header> Here's some valid html </header>"
+@app.route("/../../")
+def work():
+    return "route 3"
 
 if __name__ == "__main__":
   app.debug = True
--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,4 +1,15 @@
+# Jude Rizzo
+# SoftDev1 pd9
+# K10: Jinja Tuning
+# 2019-09-24
+
+# Pair Programming
+# Team Name: Red Notebook
+# Roster: Brandon Chen, Jude Rizzo
+
+
 from flask import Flask, render_template
+#importing the libraries
 import random, csv
 app = Flask(__name__)
 
@@ -8,14 +19,20 @@ def randomOccupation():
     with open('occupations.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         linenum = 0
+        #seperated by commas, so we tell them by their "delimeter"
         for rows in reader:
             if linenum != 0:
+                #associating the percentages in the csv
                 allOccupation[rows[0]] = float(rows[1])
+                
             linenum += 1
         counter = 0
+
         randomnum = random.uniform(0,allOccupation.get("Total"))
+
         for key, value in allOccupation.items():
             counter += value
+            #up the counter
             if randomnum <= counter:
                 return key
 
@@ -25,12 +42,15 @@ def randomOccupation():
 @app.route("/occupyflaskst")
 def getAJob():
     return render_template(
+    #set itup with the template
         'template.html',
         randOccup = randomOccupation(),
         bunch = allOccupation
+
     )
 
 
 if __name__ == "__main__":
   app.debug = True
+  #run the main code
   app.run()
