9e997d5d67e7184cf92382848fa67a7ea37de09f
Jude Rizzo <jrizzo00@stuy.edu>
2019-10-07 08:53:51 -0400

    updated

---
+++ 16_flasherit/app.py
@@ -0,0 +1,58 @@
+#Team Giraffe - Jude Rizzo, Calvin Chu
+#SoftDev1 pd9
+#K15 - Login
+#2019-10-02
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Instantiates a variable to contain a potential error message
+error = ""
+
+
+# Root serves as our login page
+@app.route("/")
+def makeForm():
+    global error
+    # If there is an error message, reset the variable and display the login page with the error message
+    if (error != ""):
+        message = error
+        error = ""
+        return render_template('app.html', e = message)
+    # Else display the login page normally
+    else :
+        return render_template('app.html')
+
+# /login route serves as welcome page
+@app.route("/login")
+def authenticate():
+    global error
+    # Hardcoded a username and password
+    use = "giraffe"
+    pas = "g"
+    # Checks to see if you are inputting values
+    if len(request.args) > 0:
+        # Stores user's inputs from login page
+        username = request.args["user"] + ""
+        password = request.args["pass"] + ""
+    # If you aren't inputting values and instead just typing /login into the URL it will redirect you back to root
+    else:
+        return redirect(url_for('makeForm'))
+
+    # If inputs match hardcoded username and password, reset error message and display welcome page with logout button
+    # **Logout button reroutes to root**
+    if(username == use and password == pas):
+        error = ""
+        return render_template('welcome.html',
+                                username = username)
+    # Else set an error message and redirect back to root
+    else:
+        error = "Wrong username or password (HINT: username = \"giraffe\", password = \"g\")"
+        return redirect(url_for('makeForm'))
+
+
+
+if __name__ == "__main__":
+    app.debug = True # Automatically updates project with save file
+    app.run()
