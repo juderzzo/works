ceb7184e0edc446b049e913806a86866d27c4fec
Jude Rizzo <jrizzo00@stuy.edu>
2020-03-17 11:38:23 -0400

    oops

---
+++ 11_mongoflask/demo.py
@@ -0,0 +1,16 @@
+import moviemango 
+from moviemango import MovieQuery
+
+
+
+
+
+
+q = MovieQuery()
+
+q.actor("michael").genre("drama")
+
+q.execute()
+
+
+q.pretty_print(limit=5, order=1, count=True)
